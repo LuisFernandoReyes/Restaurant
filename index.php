b68f180db23d3b45eac580fd3742efe113e369de
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-01-03 16:57:41 -0600

    Adding background

--- index.php
+++ index.php
@@ -16,33 +16,52 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
 
-            <a class="navbar-brand" href="#">Restaurante</a>
+            <a class="navbar-brand" href="#">Restaurante El Poderoso</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#" aria-current="page">Nav 1 <span class="visually-hidden">(current)</span></a>
+                        <a class="nav-link" href="#Inicio">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Nav 2</a>
+                        <a class="nav-link" href="#Menu">Menú del día</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Chefs">Chefs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Testimonios">Testimonios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Contacto">Contacto</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
+    <section id="banner" class="container-fluid p-0">
+        <div class="banner-img" style="position:relative; background:url('images/slider-image1.jpg') center/cover no-repeat; height: 400px;">
+            <div class="banner-text" style="position:absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; color: black;">
+            <h1>El Poderoso</h1>
+            <p>El Restaurant con el mejor sabor</p>
+            <a href="#Menu" class="btn btn-primary">Mostrar menu</a>
+            </div>
+        </div>
+    </section>
+
     <header>
         <!-- place navbar here -->
     </header>
     <main>
-
+        <p>Parte central</p>
     </main>
-    <footer>
-        <!-- place footer here -->
+    <footer class="bg-dark text-light text-center">
+        <p>&copy; 2024 Restaurante el Poderoso. Todos los derechos Reservados.</p>
     </footer>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
