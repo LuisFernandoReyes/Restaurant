31d143dc1807812598a05b6595a681e2d371e8d1
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-01-03 23:04:38 -0600

    Make cards for chefs

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
 
     <!-- Bootstrap CSS v5.3.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"/>
 </head>
 
 <body>
-
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-
             <a class="navbar-brand" href="#">Restaurante El Poderoso</a>
-
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
@@ -46,16 +46,81 @@
 
     <section id="banner" class="container-fluid p-0">
         <div class="banner-img" style="position:relative; background:url('images/slider-image1.jpg') center/cover no-repeat; height: 400px;">
-            <div class="banner-text" style="position:absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; color: black;">
-            <h1>El Poderoso</h1>
-            <p>El Restaurant con el mejor sabor</p>
-            <a href="#Menu" class="btn btn-primary">Mostrar menu</a>
+            <div class="banner-text" style="position:absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; color: green;">
+                <h1>El Poderoso</h1>
+                <p>El Restaurant con el mejor sabor</p>
+                <a href="#Menu" class="btn btn-primary">Mostrar menu</a>
             </div>
         </div>
     </section>
 
+    <section id="id" class="container mt-4 text-center">
+        <div class="jumbotron bg-dark text-white">
+            <br>
+                <h2>Bienvenido al mejor Restaurant</h2>
+                <p>
+                    Descubre una gran experiencia al probar nuestros deliciosos platillos
+                </p>
+                <br>
+        </div>
+    </section>
+
+    <section id="#Chefs" class="container mt-4 text-center">
+        <h2>Nuestros chefs</h2>
+        <div class="row ">
+            <div class="col-md-4">
+                <div class="card"> 
+                    <img src="images/colaboradores/team-colaborador1.jpg" class="card-img-top" alt="chef1" />
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 1</h5>
+                        <p class="card-text">Chef José</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>                            
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4">
+                <div class="card"> 
+                    <img src="images/colaboradores/team-colaborador2.jpg" class="card-img-top" alt="chef2" />
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 1</h5>
+                        <p class="card-text">Chef José</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>                            
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4">
+                <div class="card"> 
+                    <img src="images/colaboradores/team-colaborador3.jpg" class="card-img-top" alt="chef3" />
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 1</h5>
+                        <p class="card-text">Chef José</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
     <header>
-        <!-- place navbar here -->
+
     </header>
     <main>
         <p>Parte central</p>
